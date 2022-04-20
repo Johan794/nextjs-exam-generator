27b4209318d38edeca6797d2f0871bde5fae2b44
CamiloCJ09 <camilocampazj@gmail.com>
2022-04-20 10:13:41 -0500

    Merge

--- pages/api/student/[id].js
+++ pages/api/student/[id].js
@@ -1,5 +1,5 @@
 
 export default function handler(req, res) {
-    res.send(`Hello ${req.query.id}`)
-    res.json(req.query.id);
+    res.send("Hello "+ req.query.id)
+    //res.json(req.query.id);
 }
